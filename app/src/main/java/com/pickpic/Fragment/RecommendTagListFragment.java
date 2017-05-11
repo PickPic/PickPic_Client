813c7c5300d03b3f6afa08aa95cdd963ad6f3551
KangSoYeon <aaa03116@gmail.com>
2017-05-11 21:29:19 +0900

    Skeleton for Search

---
+++ app/src/main/java/com/pickpic/Fragment/RecommendTagListFragment.java
@@ -0,0 +1,15 @@
+package com.pickpic.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.arseneholmes.pickpic.R;
+
+public class RecommendTagListFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_recommend_tag_list);
+    }
+}
---
+++ app/src/main/java/com/pickpic/Fragment/SearchResultFragment.java
@@ -0,0 +1,15 @@
+package com.pickpic.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.arseneholmes.pickpic.R;
+
+public class SearchResultFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search_result);
+    }
+}
